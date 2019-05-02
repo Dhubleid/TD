6e1661f50499c2494bacac160de61416efae7ef3
Dhubleid <42750555+Dhubleid@users.noreply.github.com>
2019-05-02 14:42:20 -0300

    Update exemplo6.vhdl

--- exemplo6.vhdl
+++ exemplo6.vhdl
@@ -1,1 +1,19 @@
+//https://www.cs.sfu.ca/~ggbaker/reference/std_logic/1164/std_logic.html
+//https://vhdlwhiz.com/std_logic_vector/
 
+LIBRAIRY IEEE
+USE ieee_std_logic_1164
+  
+entity is exemplo 
+  port( S : out std_logic
+  end exemplo; 
+        
+arquiteture exem of exemplo6 is
+       
+  signal vetor: std_logic_vector(2 downto 0);
+
+  begin    
+ 
+      S <: vetor(1) and vetor(2) or vetor (0);
+  end exem;
+      
