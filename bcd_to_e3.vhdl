c21e5e79becd7e396bd338ebcb51f5045c2797ac
Dhubleid <42750555+Dhubleid@users.noreply.github.com>
2019-05-02 23:10:36 -0300

    Update bcd_to_e3.vhdl

--- bcd_to_e3.vhdl
+++ bcd_to_e3.vhdl
@@ -4,7 +4,7 @@ use ieee.std_logic_1164.all;
 entity bcd_to_e3 is
 	
 	port(	entrada: in std_logic_vector(3 downto 0);
-			saida: out std_logic_vector(3 downto 0)
+		saida: out std_logic_vector(3 downto 0)
 	);
 	
 end bcd_to_e3;
