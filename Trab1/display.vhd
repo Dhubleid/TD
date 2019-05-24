ce13784ac3da59384ca4d83492944d96184c69e1
Dhubleid <42750555+Dhubleid@users.noreply.github.com>
2019-05-24 05:18:18 -0300

    Update display.vhd

--- Trab1/display.vhd
+++ Trab1/display.vhd
@@ -6,14 +6,17 @@ entity display is
 	port( entrada : in std_logic_vector (3 downto 0);
 			saidac2 : out std_logic_vector (6 downto 0);
 			saida0 : out std_logic_vector (6 downto 0);
-			saida0 : out std_logic_vector (6 downto 0);
+			saida1 : out std_logic_vector (6 downto 0);
 			sinal : out out std_logic_vector (6 downto 0)
 	);
 end display;
 
 
 architecture estrut1 of display is
-signal comp2
+
+signal tempc2, temps0, temps1 : std_logic_vector (3 downto 0);
+signal tempS: std_logic_vector (7 downto 0);
+
 
 component ConversorBCDcomp2 is
 	port( entrada : in std_logic_vector (3 downto 0);
@@ -37,7 +40,16 @@ end BCD_to_seg7;
 
 	with entrada(3) select sinal <=
 		"0000001" when '1';
-	BCDc2: ConversorBCDcomp2 port map(entrada => entrada, 
+	
+	BCDc2: ConversorBCDcomp2 port map(entrada => entrada, saida => tempc2);
+	
+	BCD: ConversorBCD port map( entrada => entrada, saida => tempS);
+	
+	c2seg7: BCD_to_seg7 port map(entrada => tempc2, saida => saidac2);
+	
+	seg7_0: BCD_to_seg7 port map(entrada => tempS(7 downto 4), saida => saida0);
+	
+	seg7_1: BCD_to_seg7 port map(entrada => tempS(3 downto 0), saida => saida1);
 
 	
 	end estrut1;
