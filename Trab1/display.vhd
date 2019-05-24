9c4b61b61da9fdf95268c92ab21b8369073d9f7c
Dhubleid <42750555+Dhubleid@users.noreply.github.com>
2019-05-24 09:38:08 -0300

    Update display.vhd

--- Trab1/display.vhd
+++ Trab1/display.vhd
@@ -1,59 +1,15 @@
 library ieee;
 
-use ieee.std_logic_1164.all;
+use iee.std_logic_1164.all;
 
-entity display is
-	port( entrada : in std_logic_vector (3 downto 0);
-			saidac2 : out std_logic_vector (6 downto 0);
-			saida0 : out std_logic_vector (6 downto 0);
-			saida1 : out std_logic_vector (6 downto 0);
-			sinal : out out std_logic_vector (6 downto 0)
-	);
-end display;
+entity estrutura is 
 
+port ( numA, numB : in std_logic_vector (3 downto 0);
+		 Mult_div: in std_logic;
+		 saida1, saida0 : out std_logic_vector (3 downto 0);
+		 overflow, zero : out std_logic
+		);
+end estrutura;
 
-architecture estrut1 of display is
-
-signal tempc2, temps0, temps1 : std_logic_vector (3 downto 0);
-signal tempS: std_logic_vector (7 downto 0);
-
-
-component ConversorBCDcomp2 is
-	port( entrada : in std_logic_vector (3 downto 0);
-			saida : out std_logic_vector (3 downto 0)
-	);
-end ConversorBCDcomp2;
-
-component ConversorBCD is
-	port( entrada : in std_logic_vector (3 downto 0);
-			saida : out std_logic_vector (7 downto 0)
-	);
-end ConversorBCD;
-
-entity BCD_to_seg7 is
-	port( entrada : in std_logic_vector (3 downto 0);
-			saida : out std_logic_vector (6 downto 0)
-	);
-end BCD_to_seg7;
-	
-	begin 
-
-	with entrada(3) select sinal <=
-		"0000001" when '1';
-	
-	BCDc2: ConversorBCDcomp2 port map(entrada => entrada, saida => tempc2);
-	
-	BCD: ConversorBCD port map( entrada => entrada, saida => tempS);
-	
-	c2seg7: BCD_to_seg7 port map(entrada => tempc2, saida => saidac2);
-	
-	seg7_0: BCD_to_seg7 port map(entrada => tempS(7 downto 4), saida => saida0);
-	
-	seg7_1: BCD_to_seg7 port map(entrada => tempS(3 downto 0), saida => saida1);
-
-	
-	end estrut1;
-		 
-		
-		
-		
+architecture bhv_estrutura of estrutura is 
+	signal 
